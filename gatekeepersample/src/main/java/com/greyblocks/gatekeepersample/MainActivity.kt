c859e8679e31ebf3c8e77f714caaa4afb668f7b9
Jose S <santiagojoed@gmail.com>
2022-07-17 21:14:19 +0800

    Updated dependencies, move to jitpack

--- gatekeepersample/src/main/java/com/greyblocks/gatekeepersample/MainActivity.kt
+++ gatekeepersample/src/main/java/com/greyblocks/gatekeepersample/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.greyblocks.gatekeepersample
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.greyblocks.gatekeeper.GateKeeper
 import com.greyblocks.gatekeepersample.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    lateinit var gateKeeper: GateKeeper
+    private lateinit var gateKeeper: GateKeeper
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
